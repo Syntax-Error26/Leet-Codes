0346b2cc8ed8193450204513ee5d6643b883308d
Komal Band <komalband2006@gmail.com>
2025-04-19 11:19:15 +0530

    Time: 0 ms (100%), Space: 42.2 MB (30.67%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,13 @@
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0;  // Pointer to place the next kept element
-
+        int k = 0;  
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
-                nums[k] = nums[i];  // Keep the element
+                nums[k] = nums[i];  
                 k++;
             }
         }
 
-        return k; // First k elements in nums are the result
+        return k; 
     }
 }
